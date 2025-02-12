f82e2ab82f2175d85c4bd4ce70547712a48cf33a
Andrius Bobbit <abobbit@gmail.com>
2025-02-12 23:23:17 +0000

    Make CPU functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const player1 = 'X';
 const player2 = 'O';
 let currentToken = player1;
-let cpuMode = false;
+let cpuMode = true;
 
 document.addEventListener('DOMContentLoaded', () => {
     const turnInformation = document.getElementById("sub-heading");
@@ -16,28 +16,25 @@ document.addEventListener('DOMContentLoaded', () => {
                 alert("Square already selected");
                 return;
             }
-
-            chosenSquare.textContent = currentToken;
-            const squareClasses = getSquareClasses(chosenSquare.className);
-            const classNodeList = squareClasses.map(group => document.querySelectorAll(`.${group}`))
-            const winningSquares = checkWin(classNodeList, currentToken)
-            if (winningSquares){
-                highlightSquares(winningSquares)
-                alert("You win");
-                return
-            }
-            checkDraw(squares) && alert("Draw");
-            currentToken = (currentToken === player1) ? player2 : player1;
-            turnInformation.innerText = updatePlayerTurn(currentToken, player1);
+            updateGame(chosenSquare, squares, turnInformation);
             if (currentToken === player2 && cpuMode) {
-                chosenSquare = pickSquare();
+                chosenSquare = pickSquare(squares);
+                setTimeout(() => updateGame(chosenSquare, squares, turnInformation), 500)
             }
 
         })
     })
 })
 
-function getSquareClasses (classes) {
+
+function pickSquare(squares) {
+    const openSquareList = Array.from(squares)
+        .filter(square => !square.textContent)
+    const chosenSquare = Math.floor(Math.random() * openSquareList.length);
+    return openSquareList[chosenSquare]
+}
+
+function getSquareClasses(classes) {
     return classes
         .replace("board-square", "")
         .replace("offcolor", "")
@@ -74,6 +71,22 @@ function highlightSquares(squares) {
         square.style.backgroundColor = 'gold';
     })
 }
+
+function updateGame(chosenSquare, squares, turnInformation) {
+    chosenSquare.textContent = currentToken;
+    const squareClasses = getSquareClasses(chosenSquare.className);
+    const classNodeList = squareClasses.map(group => document.querySelectorAll(`.${group}`));
+    const winningSquares = checkWin(classNodeList, currentToken);
+    if (winningSquares){
+        highlightSquares(winningSquares)
+        alert("You win");
+        return
+    }
+    checkDraw(squares) && alert("Draw");
+    currentToken = (currentToken === player1) ? player2 : player1;
+    turnInformation.innerText = updatePlayerTurn(currentToken, player1);
+}
+
 
 
 // Wait for click
